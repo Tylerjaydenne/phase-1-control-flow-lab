850f6c14f20c6e83b12757e43c40c99f4501de64
Tylerjaydenne <tylerjaydenne127@gmail.com>
2022-12-12 13:48:00 -0500

    commit tree 329525db1a9c3b843a904efcdd7831d2fe6067dc

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(feet) {
+   if (feet <= 400){
+ return ('This one is on me!');
+ }  else if (feet >= 400 && feet < 2000); {
+  return ('That will be twenty bucks.');
+ }
+  // else if (feet >= 2499); {
+  // return ('I will gladly take your thirty bucks.');
+ }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+// }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+  
+
+
+
+// function ternaryCheckCity(){
+//   // Write your code here!
+// }
+
+// function switchOnCharmFromTip(){
+//   // Write your code here!
+// }
